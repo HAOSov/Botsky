340e4264ff8c559cd1a197b83571249ddd86438e
HAOSov <6572533+HAOSov@users.noreply.github.com>
2024-11-08 17:29:20 -0700

    Update botsky.py

--- botsky.py
+++ botsky.py
@@ -4,3 +4,8 @@ import traceback
 from discord_webhook import DiscordWebhook
 from reusable.string_functions import splash
 
+def login_to_bluesky():
+    client.login(BLUESKY_USERNAME, BLUESKY_PASSWORD)
+
+def followers_list():
+    follows = []
